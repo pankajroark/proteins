338ced3ba96a08d1105607a434ef791dbceb2d05
Pankaj Gupta <pankajg@twitter.com>
2014-03-29 21:57:48 -0700

    Hook in the functionality of reading proteins from file and autocomplete on them.

--- ProteinCounts/UCSFViewController.m
+++ ProteinCounts/UCSFViewController.m
@@ -9,16 +9,43 @@
 #import "UCSFViewController.h"
 
 @interface UCSFViewController ()
-
+- (void) loadData;
 @end
 
 @implementation UCSFViewController
+
+NSMutableDictionary* dict;
+
+NSString *content;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     _goBtn.layer.cornerRadius = 4;
 	// Do any additional setup after loading the view, typically from a nib.
+    [self loadData];
+}
+
+- (void) loadData
+{
+    NSLog(@"load data called");
+    NSString* csvPath = [[NSBundle mainBundle] pathForResource:@"protein_synthesis_rate" ofType:@"csv"];
+    NSLog(@"csv path %@", csvPath);
+    content = [NSString stringWithContentsOfFile:csvPath
+                                                  encoding:NSUTF8StringEncoding
+                                                     error:NULL];
+    NSLog(@"%@", content);
+    dict = [[NSMutableDictionary alloc] init];
+    NSArray* allLinedStrings =
+    [content componentsSeparatedByCharactersInSet:
+     [NSCharacterSet newlineCharacterSet]];
+    for (NSString* line in allLinedStrings) {
+        NSArray* parts =
+        [line componentsSeparatedByCharactersInSet:
+         [NSCharacterSet characterSetWithCharactersInString:@","]];
+        [dict setValue:parts forKey:[parts objectAtIndex:0]];
+    }
+    NSLog(@"Dictionary %@", dict);
 }
 
 - (void)didReceiveMemoryWarning
@@ -32,7 +59,14 @@
  possibleCompletionsForString:(NSString *)string
             completionHandler:(void (^)(NSArray *))handler
 {
-    handler(@[@"rbn", @"rbp", @"some", @"here", @"rbx"]);
+    NSArray* proteins = [dict allKeys];
+    NSMutableArray* filtered = [[NSMutableArray alloc] init];
+    for(NSString* protein in proteins) {
+        if([protein hasPrefix:string]) {
+            [filtered addObject:protein];
+        }
+    }
+    handler(filtered);
 }
 
 
